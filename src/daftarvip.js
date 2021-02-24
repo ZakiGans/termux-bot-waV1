5a5f5799d29f74b7bf75439718484cbe5dff8ea5
ZakiAhsan <79066165+ZakiGans@users.noreply.github.com>
2021-02-24 16:44:25 +0700

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,17 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*HARGA DAFTAR VIP :*
+-Rp. 0K > Akses Fitur ViP
+-Rp. 0K > Fitur VIP + Masukin Bot KeGrup Kalian!
+
+*JIKA INGIN DAFTAR VIP :*
+*Chat Owner BOT :*
+_wa.me/6283144780782  atau ketik *${prefix}owner*
+
+*NOTE*
+
+JANGAN LUPA SUBSCRIBE CHANNEL MikazuMZ
+`
+}
+exports.daftarvip = daftarvip
