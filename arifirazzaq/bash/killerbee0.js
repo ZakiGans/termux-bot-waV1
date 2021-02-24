b8f4b388af326b0dbe7689ba58d5d9711bf78f41
ZakiAhsan <79066165+ZakiGans@users.noreply.github.com>
2021-02-24 16:46:43 +0700

    Add files via upload

---
+++ arifirazzaq/bash/killerbee0.js
@@ -0,0 +1,26 @@
+const killerbee0 = (prefix) => { 
+
+	return `
+
+	*note:*
+	❗ Jika Anda Menonaktifkan Fitur Killerbee \nMaka Anda Harus Menonaktifkan fitur Welcome, jika tidak? \nMaka Fitur Killerbee \n*Akan terus aktif dan tidak akan bekerja walaupun sudah Di Nonaktifkan*
+	
+	*kegunaan:*
+	❗ *Jadi Fungsi Killerbee Ini, Sama Seperti Fungsi Welcome!* \n*cuma, Ini Adalah Autetikasi Pihak Ke 3 nya, Jadi Fungsinya Itu Semacam Phising!* \n\n*Singkat Penjelasan,* Intinya Setiap Orang Yang Keluar Dari Grup, Bot Bakal Kirim Laporan Palsu Ke Pihak WhatsApp Dengan Jumlah 3× laporan Palsu Secara Beruntun \n\n*Fitur Ini Cocok Di Aktifkan Untuk Orang Yang Keluar Masuk Di Grup Kamu, Semakin Sering Dia Keluar Masuk, Maka Semakin Cepat Akun WhatsApp Dia Ke banned!*
+
+
+
+❌ *cara menonaktifkan*
+*ketik : /killerbee <spasi> 0*
+*contoh : /killerbee 0*
+
+✔️ *cara mengaktifkan*
+*ketik : /killerbee(satu) <tanpa spasi>*
+*contoh : /killerbee1*
+
+`
+
+}
+
+exports.killerbee0 = killerbee0
+
